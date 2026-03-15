1015e6d601872f637925ecb8e709fdbeada4d494
jubayerjubu <jubayerislamjubu4@gmail.com>
2026-03-15 11:30:17 +0300

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -32,7 +32,7 @@ module.exports.languages = {
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: %8
 ┃ 🤖 Bot Name: %9
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 𝐉𝐮𝐛𝐚𝐲𝐞𝐫 𝐁𝐚𝐛𝐲
 ╰━━━━━━━━━━━━━━━━╯`,
         "helpList": "[ There are %1 commands. Use: \"%2help commandName\" to view more. ]",
         "user": "User",
@@ -43,7 +43,7 @@ module.exports.languages = {
 
 // 🔹 এখানে আপনার ফটো Imgur লিংক করে বসাবেন ✅
 const helpImages = [
-    "https://i.imgur.com/sxSn1K3.jpeg",
+    "https://imgur.com/a/0JqwGf0",
     "https://i.imgur.com/8WvpgUL.jpeg",
     "https://i.imgur.com/8WvpgUL.jpeg",
     "https://i.imgur.com/sxSn1K3.jpeg"
@@ -110,7 +110,7 @@ module.exports.run = function ({ api, event, args, getText }) {
             command.config.commandCategory || "Unknown",  
             command.config.cooldowns || 0,  
             prefix,  
-            global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"  
+            global.config.BOTNAME || "𝐉𝐮𝐛𝐮 𝐗 𝐁𝐨𝐭"  
         );  
 
         downloadImages(files => {  
@@ -144,7 +144,7 @@ ${msg}
 ┣━━━━━━━━━━━━━━━━┫
 ┃ ⚙ Prefix: ${prefix}
 ┃ 🤖 Bot Name: ${global.config.BOTNAME || "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭"}
-┃ 👑 Owner: 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓 𝐒𝐀𝐇𝐔
+┃ 👑 Owner: 𝐉𝐮𝐛𝐚𝐲𝐞𝐫 𝐁𝐚𝐛𝐲
 ╰━━━━━━━━━━━━━━━━╯`;
 
     downloadImages(files => {  
